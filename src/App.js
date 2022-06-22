dc1e377db248e4bd839a77a252ea811e14642aa2
Loly Maluf <lolymaluf@gmail.com>
2022-06-22 19:07:19 -0300

    Adiciona inicio requisito 1 pada dar fiz no teste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+import { Login, Wallet } from './pages/Login'
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return <div>Hello, TrybeWallet!
+  <Switch>
+    <Route exact path = '/'><Login /></Route>
+    <Route exact path = '/carteira'><Wallet/></Route>
+  </Switch>
+  </div>;
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,31 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    const minPassword = 6
+    const { email, password } = this.state;
+    return 
+    <div>Login
+      <form>
+        <input 
+        type="email"
+        data-testid="email-input"
+        value={ email }
+        required
+        />
+
+        <input 
+        type="password"
+        data-testid="password-input"
+        value={ password }
+        required
+        />
+
+        <button 
+        type="submit"
+        disabled={ password.length < minPassword }
+        >Entrar</button>
+      </form>
+    </div>;
   }
 }
 
