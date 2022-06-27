0af1b9612a118cf61f13c6707961b91c24112895
Loly Maluf <lolymaluf@gmail.com>
2022-06-27 18:25:24 -0300

    adiciona parte do requisito 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,29 @@
 import React from 'react';
 
 class Login extends React.Component {
+
+  state = { 
+    email: '',
+    password: '',
+    isButtonEnabled: 'false',
+  }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState(({ [name]: value}));
+    this.checkButton;
+  }
+  
+  checkButton = () => {
+    const { email, password } = this.state;
+    if (email.includes('@')
+    || email.includes('.com')) {
+      this.setState({isButtonEnabled: false})
+    }
+    else {
+      this.setState({isButtonEnabled: true})
+    }
+  }
   render() {
     const minPassword = 6
     const { email, password } = this.state;
@@ -12,6 +35,7 @@ class Login extends React.Component {
         data-testid="email-input"
         value={ email }
         required
+        onChange={ this.onInputChange }
         />
 
         <input 
@@ -19,6 +43,7 @@ class Login extends React.Component {
         data-testid="password-input"
         value={ password }
         required
+        onChange={ this.onInputChange }
         />
 
         <button 
